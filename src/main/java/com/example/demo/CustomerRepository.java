252d7bf46c0a7ede2de7271293a7676c65d3eaa2
ryuska <yromanova2010@gmail.com>
2019-05-22 13:30:01 -0400

    Update

--- src/main/java/com/example/demo/CustomerRepository.java
+++ src/main/java/com/example/demo/CustomerRepository.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CustomerRepository extends CrudRepository<Customer,Long> {
-    long countByStateID(long stateId);
+//    long countByStateID(long stateId);
     Iterable<Customer> findAllByLastNameContainingIgnoreCase(String lastName);
     //Iterable<Customer> findAllByCityAndState(String city, String state);
 }
--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -3,9 +3,13 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
 
 @Controller
 public class HomeController {
@@ -13,18 +17,30 @@ public class HomeController {
     CustomerRepository customerRepository;
 
     @RequestMapping("/")
-    public String index(Model model){
+    public String HomePage(Model model) {
 
-        String lastName = "Smith%";
-        ArrayList<Customer> results =(ArrayList<Customer>)
-                customerRepository.findAllByLastNameContainingIgnoreCase(lastName);
-
-        long stateId = 2;
-        long total = customerRepository.countByStateID(stateId);
-
-        model.addAttribute("state",stateId);
-        model.addAttribute("total",total);
-        model.addAttribute("results", results);
+        model.addAttribute("customers", customerRepository.findAll());
         return "index";
+
+    }
+
+
+    @RequestMapping("/search")
+    public String loadFormPage(@RequestParam("lastname") String lastName,
+    Model model){
+        model.addAttribute("customers", customerRepository.findAllByLastNameContainingIgnoreCase(lastName));
+        return "customer";
+
+//        String lastName = "Smith%";
+//        ArrayList<Customer> results =(ArrayList<Customer>)
+//                customerRepository.findAllByLastNameContainingIgnoreCase(lastName);
+//
+//        long stateId = 2;
+//        long total = customerRepository.countByStateID(stateId);
+//
+//        model.addAttribute("state",stateId);
+//        model.addAttribute("total",total);
+//        model.addAttribute("results", results);
+//        return "index";
     }
 }
